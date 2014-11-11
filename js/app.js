8406d10d9fe894db3cde0523e79436f65408b005
ph0rest82 <ph0rest82@gmail.com>
2014-11-11 14:59:50 +0100

    Added owlcarousel; page is finished; DOTO: refactoring!!!

--- js/app.js
+++ js/app.js
@@ -1,8 +1,57 @@
-$('#select-dropdown').on('click', function(){
-	$('.checklist').slideToggle(200);
-});
+$(document).ready(function() {
+
+  /*****************************************************
+    OWLCAROUSEL PLUGIN
+  ******************************************************/
+
+	var owl = $("#owl-example");
+     
+	owl.owlCarousel({
+   				
+   		itemsDesktop: [1170],
+   		singleItem:true,
+   		slideSpeed: 300,
+   		pagination: false
+  });
+   	
+  $(".next").click(function(){
+    owl.trigger('owl.next');
+  });
+  		
+	$(".prev").click(function(){
+   	owl.trigger('owl.prev');
+ 	});
+
+  /*****************************************************
+    MULTIPLE DROPDOWN WITH CHECKBOXES
+  ******************************************************/
+
+ 	$('#select-dropdown').on('click', function(){
+		$('.checklist').slideToggle(200);
+	});
   
-$('.checklist-btn').on('click', function(e){
-  	e.preventDefault();
-  	$('.checklist').slideUp(200);
+ 	$('.checklist-btn').on('click', function(e){
+ 		e.preventDefault();
+ 		$('.checklist').slideUp(200);
+	});
+
+
+  /*****************************************************
+    ACCORDION
+  ******************************************************/
+
+  $('.accordion').on('click', '.title', function(e) {
+    
+    var $this = $(this),
+        $accordion = $this.parent();
+    e.preventDefault();
+    $this.find('i').toggleClass('fa-minus-square');
+    $this.next().slideToggle();
+    $accordion.siblings()
+      .find('.content').slideUp().end()
+      .find('i').removeClass('fa-minus-square');
+  });
+
 });
+
+
