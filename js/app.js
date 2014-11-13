eb943cd2178f3591976298d2ff0a0fa9030a275c
ph0rest82 <ph0rest82@gmail.com>
2014-11-13 11:20:14 +0100

    Finished

--- js/app.js
+++ js/app.js
@@ -1,58 +1,61 @@
-$(document).ready(function() {
+(function(){
+  $(function() {
 
-  /*****************************************************
-    MULTIPLE DROPDOWN WITH CHECKBOXES
-  ******************************************************/
+    /*****************************************************
+      MULTIPLE DROPDOWN WITH CHECKBOXES
+    ******************************************************/
 
- 	$('#select-dropdown').on('click', function(){
-		$('.checklist').slideToggle(200);
-	});
-  
- 	$('.checklist').on('click','.btn', function(e){
- 		e.preventDefault();
- 		$('.checklist').slideUp(200);
-	});
-
-
-  /*****************************************************
-    ACCORDION
-  ******************************************************/
-
-  $('.accordion').on('click', '.title', function(e) {
+   	$('#select-dropdown').on('click', function(){
+  		$('.checklist').slideToggle(200);
+  	});
     
-    var $this = $(this),
-        $accordion = $this.parent();
-    e.preventDefault();
-    $this.find('i').toggleClass('fa-minus-square');
-    $this.next().slideToggle();
-    $accordion.siblings()
-      .find('.content').slideUp().end()
-      .find('i').removeClass('fa-minus-square');
-  });
-  
+   	$('.checklist').on('click','.btn', function(e){
+   		e.preventDefault();
+   		$('.checklist').slideUp(200);
+  	});
 
-  /*****************************************************
-    OWLCAROUSEL PLUGIN
-  ******************************************************/
 
-  var owl = $("#owl-example");
-     
-  owl.owlCarousel({
-          
-      itemsDesktop: [1170],
-      singleItem:true,
-      slideSpeed: 300,
-      pagination: false
-  });
-    
-  $(".next").click(function(){
-    owl.trigger('owl.next');
-  });
+    /*****************************************************
+      ACCORDION
+    ******************************************************/
+
+    $('.accordion').on('click', '.title', function(e) {
       
-  $(".prev").click(function(){
-    owl.trigger('owl.prev');
+      var $this = $(this),
+          $accordion = $this.parent();
+      e.preventDefault();
+      $this.find('i').toggleClass('fa-minus-square');
+      $this.next().slideToggle();
+      $accordion.siblings()
+        .find('.content').slideUp().end()
+        .find('i').removeClass('fa-minus-square');
+    });
+    
+
+    /*****************************************************
+      OWLCAROUSEL PLUGIN
+    ******************************************************/
+
+    var owl = $("#owl-example");
+       
+    owl.owlCarousel({
+            
+        itemsDesktop: [1170],
+        singleItem:true,
+        slideSpeed: 300,
+        pagination: false
+    });
+      
+    $(".next").click(function(){
+      owl.trigger('owl.next');
+    });
+        
+    $(".prev").click(function(){
+      owl.trigger('owl.prev');
+    });
+
   });
 
-});
+})();
 
 
